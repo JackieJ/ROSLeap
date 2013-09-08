63bc524984827a97fa2f165abc503282d6d059a1
Jackie Jin <liveaeon@gmail.com>
2013-09-07 23:08:45 -0700

    setup.sh updated

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo ""
-echo "export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$PWD" >> ~/.bashrc
-echo "copying..."
-sleep 1
-source ~/\.bashrc
+if which leapd > /dev/null; then
+    echo "the Leapmotion driver already exists."
+else
+    echo "installing the Leapmotion driver"
+    if $(uname -m | grep '64'); then
+        sudo dpkg --install ./leapsetup/Leap-0.8.0-x64.deb
+    else
+        sudo dpkg --install ./leapsetup/Leap-0.8.0-x86.deb
+    fi
+fi
