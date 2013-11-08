cd44badd4c05c07fa13025986335ebf86d8839b0
Jackie Jin <liveaeon@gmail.com>
2013-11-08 00:07:13 -0800

    rosardrone application

---
+++ rosleap_applications/src/LeapARDrone/LeapARDrone.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#Author: Jackie Jin
+#Leapmotion,Inc. reserves all the rights to the Leapmotion Python SDK
+#used as part of the code
+
+from numpy import *
+
+import rospy
+import roslib; roslib.load_manifest("geometry_msgs")
+from geometry_msgs.msg import *
+
+
