737ebf603140dfd17be14dffe2290e8404b28de0
Jackie Jin <liveaeon@gmail.com>
2013-11-08 18:09:02 -0800

    leapardrone driver

--- rosleap_applications/src/LeapARDrone/LeapARDrone.py
+++ rosleap_applications/src/LeapARDrone/LeapARDrone.py
@@ -7,8 +7,47 @@ from numpy import *
 
 import rospy
 import roslib; roslib.load_manifest("geometry_msgs")
+import roslib; roslib.load_manifest("rosleap_msg")
 from geometry_msgs.msg import *
+from rosleap_msg.msg import *
 
-class LeapARDrone():
-    def on_init(self):
+#helper functions
+def rosprint(string):
+    rospy.loginfo(string)
+
+class LeapARDrone:
+    def __init__(self):
         rospy.init_node('LeapARNode')
+        rosprint('starting leapmotion-ardrone control...')
+        #Topic Handlers
+        self.LeapARPub = rospy.Publisher('/cmd_vel', Twist)
+        self.TwistMsg = Twist()
+        
+        
+    def run(self):
+        #linear control
+        rospy.Subscriber('/leap', LeapmotionMsg, self.linearControl)
+        rospy.spin()
+    
+    def linearControl(self, data):
+        #rosprint('[LeapAR][LinearControl] processing data...')
+        
+        pass
+    
+    def angularControl(self, data):
+        pass
+    
+    def fusedControl(self, data):
+        pass
+    
+    def publish(self):
+        self.LeapARPub.publish(TwistMsg)        
+        pass
+    
+
+if __name__ == "__main__":
+    try:
+        LeapAR = LeapARDrone()
+        LeapAR.run()
+    except rospy.ROSInterruptException:
+        rosprint('leapmotion control interrupted!')
