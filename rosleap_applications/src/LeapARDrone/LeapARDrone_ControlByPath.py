0197629561e5221e1d2bbeb9c33135701f46b292
Jackie Jin <liveaeon@gmail.com>
2014-02-07 19:51:41 -0800

    vel mode added

--- rosleap_applications/src/LeapARDrone/LeapARDrone_ControlByPath.py
+++ rosleap_applications/src/LeapARDrone/LeapARDrone_ControlByPath.py
@@ -3,16 +3,14 @@
 #Leapmotion,Inc. reserves all the rights to the Leapmotion Python SDK
 #used as part of the code
 
-from numpy import *
-
 import rospy
-import rosbag
 import roslib; roslib.load_manifest("geometry_msgs")
 import roslib; roslib.load_manifest("rosleap_msg")
 from std_msgs.msg import *
 from geometry_msgs.msg import *
 from rosleap_msg.msg import *
 import math
+import numpy
 from collections import deque
 
 #helper functions
@@ -25,29 +23,19 @@ class LeapARDroneControlByPath:
         rosprint('[ControlByPath]starting leapmotion-ardrone control...')
         #Topic Handlers
         self.LeapARPub = rospy.Publisher('/cmd_vel', Twist)
-        self.pathMsgs = deque() #queue of path drawn by the finger
-        self.twistMsgs = deque() #queue of twist msgs from path drawn
-        self.pathBag = rosbag.Bag('path.bag', 'w')
-        self.twistBag = rosbag.Bag('twist.bag', 'w')
-    
+       
         #constants
         self.QUEUESIZE = 10
-        self.POSTHRESHHOLD = 5
+        self.VELTHRESHHOLD = 1500
+        self.MAXVAL = 99999
+
+        #previous position
+        self.prevPos = [self.MAXVAL, self.MAXVAL, self.MAXVAL]
         
     def run(self):
         #test
         rospy.Subscriber('/leap', LeapmotionMsg, self.pathVizTest)
         rospy.spin()
-    
-    #helper functions
-    def isVectorAboveThreshhold(self, vector, threshhold):
-        for mem in vector:
-            if mem > threshhold:
-                return True
-        return False
-    
-    def twistMsgGenerator(self, bag):
-        pass
     
     def pathVizTest(self, data):
         twistMsg = Twist()
@@ -60,21 +48,38 @@ class LeapARDroneControlByPath:
             if numberofFinger != 0:
                 #linear twist, x,y,z
                 averageLinearTwist = [0,0,0]
+                averageLinearVel = [0,0,0]
                 for finger in fingers:
+                    vel_vector = finger.tip_velocity.cartesian                    
                     pos_vector = finger.tip_position.cartesian
+                    
                     averageLinearTwist[0] += pos_vector[0]
                     averageLinearTwist[1] += pos_vector[1]
                     averageLinearTwist[2] += pos_vector[2]
-                #average twist
+                    
+                    averageLinearVel[0] += vel_vector[0]
+                    averageLinearVel[1] += vel_vector[1]
+                    averageLinearVel[2] += vel_vector[2]
+                    
+                currentTwist = [0,0,0]
                 for index in range(0,3):
                     averageLinearTwist[index] = averageLinearTwist[index]/numberofFinger
-                #write to the twist bag
-                
-                
+                    averageLinearVel[index] = averageLinearVel[index]/numberofFinger
+                    if numpy.mean(self.prevPos) == self.MAXVAL:
+                        self.prevPos[index] = averageLinearTwist[index]
+                    else:
+                        currentTwist[index] = averageLinearTwist[index] - self.prevPos[index]
+            else:
+                self.prevPos = [self.MAXVAL, self.MAXVAL, self.MAXVAL]
         else:
-            #replay data to generate twist msg
-            twistMsgGenerator(self.pathBag)
-    
+            #reset prevPos
+            self.prevPos = [self.MAXVAL, self.MAXVAL, self.MAXVAL]
+            
+    def velPath(self, vel_vector):
+        #map: x --> -y ; y --> z ; z --> -x
+        
+        pass
+            
     def publish(self, data):
         self.LeapARPub.publish(data)        
             
